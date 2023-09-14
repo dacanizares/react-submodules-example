d137de637eeb209dc1aa7442c812a92083e6cd6b
dacanizares <dacanizares@outlook.com>
2023-09-13 23:09:32 -0500

    Updated lib

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import SharedComponent from './lib/SharedComponent.jsx';
+import AnotherComponent from './lib/AnotherComponent.jsx';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <header className="App-header">
         <h1>My app</h1>    
         <SharedComponent />
+        <AnotherComponent />
       </header>
     </div>
   );
